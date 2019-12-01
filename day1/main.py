503fb1a192c54eb03b115b2c05d5f2da413ba61c
LSchultebraucks <L.Schultebraucks@t-online.de>
2019-12-01 18:59:17 +0100

    Day1 - Part Two

--- day1/main.py
+++ day1/main.py
@@ -1,5 +1,6 @@
 def main():
     part_one()
+    part_two()
 
 
 def part_one():
@@ -9,6 +10,24 @@ def part_one():
 
     for current in input_file:
         sum_fuel_requirements = sum_fuel_requirements + (int(int(current) / 3) - 2)
+
+    print(sum_fuel_requirements)
+
+
+def part_two():
+    input_file = open('input.txt', 'r')
+
+    sum_fuel_requirements = 0
+
+    for current in input_file:
+        current_fuel = (int(int(current) / 3) - 2)
+        sum_fuels = current_fuel
+        while current_fuel > 0:
+            current_fuel = (int(int(current_fuel) / 3) - 2)
+            if current_fuel > 0:
+                sum_fuels = sum_fuels + current_fuel
+
+        sum_fuel_requirements = sum_fuel_requirements + sum_fuels
 
     print(sum_fuel_requirements)
 
