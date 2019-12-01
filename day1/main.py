7280ad29bbcd7cb99f429dd5a91572ba76e8b500
LSchultebraucks <L.Schultebraucks@t-online.de>
2019-12-01 19:05:01 +0100

    refactoring

--- day1/main.py
+++ day1/main.py
@@ -1,6 +1,6 @@
 def main():
-    part_one()
-    part_two()
+    print(part_one())
+    print(part_two())
 
 
 def part_one():
@@ -9,9 +9,9 @@ def part_one():
     sum_fuel_requirements = 0
 
     for current in input_file:
-        sum_fuel_requirements = sum_fuel_requirements + (int(int(current) / 3) - 2)
+        sum_fuel_requirements = sum_fuel_requirements + calc_fuel(int(current))
 
-    print(sum_fuel_requirements)
+    return sum_fuel_requirements
 
 
 def part_two():
@@ -20,16 +20,20 @@ def part_two():
     sum_fuel_requirements = 0
 
     for current in input_file:
-        current_fuel = (int(int(current) / 3) - 2)
+        current_fuel = calc_fuel(int(current))
         sum_fuels = current_fuel
         while current_fuel > 0:
-            current_fuel = (int(int(current_fuel) / 3) - 2)
+            current_fuel = calc_fuel(current_fuel)
             if current_fuel > 0:
                 sum_fuels = sum_fuels + current_fuel
 
         sum_fuel_requirements = sum_fuel_requirements + sum_fuels
 
-    print(sum_fuel_requirements)
+    return sum_fuel_requirements
+
+
+def calc_fuel(mass):
+    return int(mass / 3) - 2
 
 
 if __name__ == '__main__':
